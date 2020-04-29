cf70ec79a95253505746159144272b790d4e4e56
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2020-04-30 01:41:55 +0300

    add-normalize

--- wav_vis.py
+++ wav_vis.py
@@ -8,7 +8,7 @@ types = {
     4: np.int32
 }
 
-namefile = r"crow\birds005.wav"
+namefile = r"meadow lark\birds013.wav"
 namefile1 = r"peacock\birds010.wav"
 
 w = wave.open(namefile, 'r')
@@ -25,7 +25,10 @@ samples1 = np.frombuffer(frames1, dtype=types[sampwidth1])
 
 # преобразование фурье
 fft = np.absolute(np.fft.fft(samples))
+fft = fft / fft.max()
+
 fft1 = np.absolute(np.fft.fft(samples1))
+fft1 = fft1 / fft1.max()
 
 # координаты для графика волны
 x_fft1 = np.absolute(np.fft.fftfreq(len(samples1), 1 / framerate1))  # вычисляет частоты
@@ -37,21 +40,21 @@ y_fft = fft
 
 # Постройка графика
 fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(20, 10))
-fig.subplots_adjust(wspace=0, hspace=0.3)
+fig.subplots_adjust(wspace=0, hspace=0.5)
 
-axes[0].set_title('Peacock FFT', fontsize=14)
+axes[0].set_title(namefile1, fontsize=14)
 axes[0].set_xlabel('Hz', fontsize=10)
 axes[0].set_ylabel('Value fourier', fontsize=10)
 axes[0].grid(True, c='lightgray', alpha=0.5)
 axes[0].plot(x_fft1, y_fft1, c="orange")
 
-axes[1].set_title('Crow FFT', fontsize=14)
+axes[1].set_title(namefile, fontsize=14)
 axes[1].set_xlabel('Hz', fontsize=10)
 axes[1].set_ylabel('Value fourier', fontsize=10)
 axes[1].grid(True, c='lightgray', alpha=0.5)
 axes[1].plot(x_fft, y_fft)
 
-axes[2].set_title('Starling + crow', fontsize=14)
+axes[2].set_title('Twise', fontsize=14)
 axes[2].set_xlabel('Hz', fontsize=10)
 axes[2].set_ylabel('Value fourier', fontsize=10)
 axes[2].grid(True, c='lightgray', alpha=0.5)
