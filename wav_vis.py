9fc6ae3557dc98ae2eb0ad565dd6828241c176e4
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2020-04-29 21:08:42 +0300

    with-Fourier

--- wav_vis.py
+++ wav_vis.py
@@ -8,20 +8,43 @@ types = {
     4: np.int32
 }
 
-namefile = "crow.wav"
+namefile = r"sound\crow.wav"
 
 w = wave.open(namefile, 'r')
 (nchannels, sampwidth, framerate, nframes, comptype, compname) = w.getparams()
-
-duration = nframes / framerate
 frames = w.readframes(nframes)
-samples = np.frombuffer(frames, dtype= types[sampwidth])
-lens = len(samples)
-x = np.arange(1, len(samples)+1)
+
+samples = np.frombuffer(frames, dtype=types[sampwidth])
+
+# преобразование фурье
+fft = np.absolute(np.fft.fft(samples))
+
+# координаты для графика волны
+x = np.arange(1, len(samples) + 1)
 y = samples
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-fig.savefig("test.png")
+# координаты для преобразования фурье
+x_fft = np.absolute(np.fft.fftfreq(len(samples), 1 / framerate))  # вычисляет частоты
+y_fft = fft
+
+# Постройка графика
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+fig.subplots_adjust(wspace=0, hspace=0.3)
+
+axes[0].set_title('Original audio', fontsize=14)
+axes[0].set_xlabel('Number of frames', fontsize=10)
+axes[0].set_ylabel('Value frame', fontsize=10)
+axes[0].grid(True, c='lightgray', alpha=0.5)
+
+axes[1].set_title('FFT', fontsize=14)
+axes[1].set_xlabel('Hz', fontsize=10)
+axes[1].set_ylabel('Value fourier', fontsize=10)
+axes[1].grid(True, c='lightgray', alpha=0.5)
+
+axes[0].plot(x, y)
+axes[1].plot(x_fft, y_fft)
+
+fig.savefig("wave.png")
 fig.show()
+
 w.close()
