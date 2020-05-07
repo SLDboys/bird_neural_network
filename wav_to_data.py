5c0eb761c8af691a2e0ea8e2026ad5e5a275897a
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2020-05-07 05:23:50 +0300

    ver1.5
    redesign file wav_to_date 50%

--- wav_to_data.py
+++ wav_to_data.py
@@ -4,12 +4,14 @@ import numpy as np
 
 
 class Date:
+
     _samples = None
     _thinned_samples = None
     _framerate = 0
     _thin_factor = 1
     _sigma_window = 15
     _fft_array = None
+    _date_array = None
 
     _types = {
         1: np.int8,
@@ -28,27 +30,33 @@ class Date:
         np.asarray(thinned_samples)  # превращает в массив нампая
         return thinned_samples
 
-    def __init__(self, namefile):
+    # Необязательные аргументы thin_factor и sigma нужны для простоты подгона значений
+    def __init__(self, namefile, thin_factor=_thin_factor, sigma=_sigma_window):
         w = wave.open(namefile, 'r')
 
+        # получение параметров из wav
         sampwidth = w.getsampwidth()
         self._framerate = w.getframerate()
         nframes = w.getnframes()
 
+        # считываем фреймы в массив и переводим в интовое значение
         frames = w.readframes(nframes)
         self._samples = np.frombuffer(frames, dtype=self._types[sampwidth])
 
         # прорежает семплы
-        self._thinned_samples = self._thinSamples(self._samples, self._thin_factor)
+        self._thinned_samples = self._thinSamples(self._samples, thin_factor)
 
         # умножение на окно
-        window = signal.gaussian(len(self._samples), std=self._sigma_window)
+        window = signal.gaussian(len(self._samples), std=sigma)
 
-        # преобразование фурье
+        # преобразование фурье (Тут функция Вадима)
         self._fft_array = np.absolute(np.fft.fft(self._samples * window))
 
         # нормировка
         self._fft_array = self._fft_array / self._fft_array.max()
+
+        # Здесь должна быть функция для уменьшения кол-ва точек вектора или PCA
+        # Результат должен записываться в _date_array
 
         w.close
 
@@ -63,3 +71,6 @@ class Date:
 
     def getFftArray(self):
         return self._fft_array
+
+    def getDateArray(self):
+        return self._date_array
